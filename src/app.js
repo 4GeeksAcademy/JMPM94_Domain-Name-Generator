b8f96c861ee8fc3553f8a1855560108095dba1cc
JMPM94 <137455695+JMPM94@users.noreply.github.com>
2023-10-09 19:02:22 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extensions = [".com", "net"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extensions.length; l++) {
+          let possibleRandomCombinations =
+            pronoun[i] + adj[j] + noun[k] + extensions[l];
+          console.log(possibleRandomCombinations);
+        }
+      }
+    }
+  }
 };
